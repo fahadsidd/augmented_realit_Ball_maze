f7ca91f622be0ad6574ddaacaeaffb5ea1df9bc4
Fahad Siddiqui <fahadsid2006@yahoo.co.in>
2018-02-28 21:43:43 -0800

    Update Ballscript.cs

--- Ballscript.cs
+++ Ballscript.cs
@@ -8,20 +8,14 @@ public class Ballscript : MonoBehaviour {
 	public GameObject endPoint;
 	public GameObject image;
 	// Use this for initialization
-	void Start () {
-		
+	void Start () {	
 	}
-	
 	// Update is called once per frame
+	// if ball fall in the direction of y more then 50 units ball position transform to spawnpoint position
 	void Update () {
 		if (transform.position.y < plane.transform.position.y - 50) {
-
 			transform.position = spawnPoint.transform.position;
-
-
-
-		}
-			
+		}		
 	}
 }
 
